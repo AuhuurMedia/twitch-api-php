d89adf9802b01223726a1b17e6883d216fa1f3d2
J.-Fabian Wenisch <dev@jfwenisch.com>
2021-02-23 19:07:16 +0100

    fix: support for latestfollowers

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ define('ACCESSTOKEN', getAccessToken());
 # or abort if it's not included
 # So make sure to add your functions to the array if you add new ones
 
-$validFunctions = array("info","topclips","user");
+$validFunctions = array("info","topclips","user","latestfollowers");
 $functName = $_REQUEST['f'];
 
 if(in_array($functName,$validFunctions))
@@ -125,8 +125,71 @@ if (curl_errno($ch)) {
 curl_close($ch);
 $json = json_decode($result); 
 $data =  $json->users[0]; 
-echo json_encode($data); 
+echo json_encode($data);
+return json_encode($data);
+}
+function getuserid()
+{
+$ch = curl_init();
+$channelname = CHANNELNAME;
+$clientid = CLIENTID;
+
+curl_setopt($ch, CURLOPT_URL, "https://api.twitch.tv/kraken/users?login={$channelname}");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+
+$headers = array();
+$headers[] = "Client-Id: {$clientid}";
+$headers[] = "Accept: application/vnd.twitchtv.v5+json";
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+$result = curl_exec($ch);
+if (curl_errno($ch)) {
+    echo 'Error:' . curl_error($ch);
+}
+curl_close($ch);
+$json = json_decode($result); 
+$data =  $json->users[0]; 
+
+$json = json_decode( json_encode($data), true);
+$userid=$json['_id'];
+return $userid;
 }
 
 
+function latestfollowers()
+{
+$ch = curl_init();
+$channelname = CHANNELNAME;
+$clientid = CLIENTID;
+$accesstoken =ACCESSTOKEN;
+
+$userid=getuserid();
+
+
+curl_setopt($ch, CURLOPT_URL, "https://api.twitch.tv/helix/users/follows?to_id={$userid}");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+
+$headers = array();
+$headers[] = "Client-Id: {$clientid}";
+$headers[] = "Authorization: Bearer {$accesstoken}";
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+$result = curl_exec($ch);
+if (curl_errno($ch)) {
+    echo 'Error:' . curl_error($ch);
+}
+curl_close($ch);
+ 
+
+$json = json_decode($result); 
+$data =  $json->data; 
+echo json_encode($data);
+return json_encode($data); 
+}
 ?>
